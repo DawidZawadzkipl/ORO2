cb5c62be0c86894b016fc11bcbceccd72ab3c17a
DawidZawadzki1 <248352@edu.p.lodz.pl>
2026-04-30 02:09:31 +0200

    Added first two methods with HQL

--- src/main/java/org/dawidz/cinema/repository/MovieRepository.java
+++ src/main/java/org/dawidz/cinema/repository/MovieRepository.java
@@ -1,7 +1,24 @@
 package org.dawidz.cinema.repository;
 
+import org.dawidz.cinema.dto.MovieDto;
 import org.dawidz.cinema.model.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+    //1.lista filmow granych w sali o danym id
+    @Query("""
+           select distinct new org.dawidz.cinema.dto.MovieDto(
+                      m.id,
+                      m.title
+           )
+          from Seans s 
+          join s.movie m
+          where s.room.id = :roomId
+          and m.active = true
+          and s.cancelled = false
+           """)
+    Page<MovieDto> findMoviesByRoomId(Long roomId, Pageable pageable);
 }
--- src/main/java/org/dawidz/cinema/repository/SeansRepository.java
+++ src/main/java/org/dawidz/cinema/repository/SeansRepository.java
@@ -1,8 +1,29 @@
 package org.dawidz.cinema.repository;
 
+import org.dawidz.cinema.dto.SeansDto;
+import org.dawidz.cinema.model.Movie;
 import org.dawidz.cinema.model.Seans;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 
 public interface SeansRepository extends JpaRepository<Seans, Long> {
-
+    //2 lista seansow granych w sali o danym id
+    @Query("""
+        select new org.dawidz.cinema.dto.SeansDto(
+            s.id,
+            m.id,
+            m.title,
+            r.id,
+            r.roomNumber,
+            s.dateTime
+        )
+        from Seans s
+        join s.movie m
+        join s.room r
+        where r.id = :roomId
+""")
+    Page<SeansDto> findSeansesByRoomId(Long roomId, Pageable pageable);
 }
